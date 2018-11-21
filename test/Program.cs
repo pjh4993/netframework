c7e2ae56cb147650235cbd8e6546ff048cf596b1
junhoPark <pjh3974@gmail.com>
2018-11-21 12:42:21 +0900

    class doesn't registered

---
+++ test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly a = Assembly.LoadFrom(@".\KHOpenAPI.dll");
+            Console.WriteLine(a);
+            Type _DKHOpenAPI = a.GetType("KHOpenAPI.KHOpenAPIClass");
+            Console.WriteLine(_DKHOpenAPI);
+            MethodInfo CommConnect = _DKHOpenAPI.GetMethod("CommConnect");
+            Console.WriteLine(CommConnect);
+            object obj = Activator.CreateInstance(_DKHOpenAPI);
+            CommConnect.Invoke(obj,null);
+        }
+    }
+}
---
+++ usingDllMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace usingDllMethod
+{
+    class Program
+    {
+        public static void Main(){
+            Assembly a = Assembly.LoadFrom(@".\KHOpenAPI.dll");
+            Console.WriteLine(a);
+            Type _DKHOpenAPI = a.GetType("KHOpenAPI._DKHOpenAPI");
+            Console.WriteLine(_DKHOpenAPI);
+            MethodInfo CommConnect = _DKHOpenAPI.GetMethod("CommConnect");
+            Console.WriteLine(CommConnect);
+            object obj = Activator.CreateInstance(_DKHOpenAPI);
+            CommConnect.Invoke(obj,null);
+        }
+    }
+}
